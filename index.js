34914daac74da60c24b18cee2dddd49c486cee48
unknown <skshihab170@gmail.com>
2024-08-17 17:07:05 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express =require('express')
 const app=express() 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port= process.env.PORT || 3004 
 require('dotenv').config()
 const cors=require('cors') 
 
 
 app.use(cors())
+app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pppehle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,10 +23,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+const productCollection=client.db('laptopdb').collection('laptopcollection')
+
+app.get('/laptop',async(req,res)=>{
+    const allData= await productCollection.find().toArray() 
+    res.send(allData)
+})
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
